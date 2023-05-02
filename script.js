7f8ba3b76758a1bf9e9e50b4cc7cad9dc9fda9b3
sudharsananr1 <115912664+sudharsananr1@users.noreply.github.com>
2023-05-02 22:50:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+let header = document.querySelector('#header');
+
+menu.onclick = () =>{
+
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+
+}
+
+window.onscroll = () =>{
+
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+
+  if(window.scrollY > 20){
+    document.querySelector('#scroll-top').classList.add('active');
+    document.querySelector('#header').classList.add('active');
+  }else{
+    document.querySelector('#scroll-top').classList.remove('active');
+  }
+
+}
+
+function loader(){
+  document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+function fadeOut(){
+  setInterval(loader, 3000);
+}
+
+window.onload = fadeOut();
+
+var className = "inverted";
+var scrollTrigger = 60;
+
+window.onscroll = function() {
+  // We add pageYOffset for compatibility with IE.
+  if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
+    document.getElementsByTagName("header")[0].classList.add(className);
+  } else {
+    document.getElementsByTagName("header")[0].classList.remove(className);
+  }
+};
